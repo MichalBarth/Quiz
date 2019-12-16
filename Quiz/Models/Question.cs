2a43971ac6872d5975ea01908dc4d210e75635ed
Michal Barth <u17.401@pslib.cloud>
2019-12-16 21:07:24 +0100

    Přidejte soubory projektu.

---
+++ Quiz/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quiz
+{
+    public class Question
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string Correct { get; set; }
+
+        [Required]
+        public string Incorrect { get; set; }
+    }
+}
---
+++ Quiz/Models/QuestionsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Quiz.Models
+{
+    public class QuestionsDbContext : DbContext
+    {
+        public QuestionsDbContext(DbContextOptions<QuestionsDbContext> opt) : base(opt) { }
+
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Question>().HasData(new Question() { Id = System.Guid.NewGuid(), Text = "How many fingers do person have?", Correct = "20", Incorrect = "10" });
+        }
+    }
+}
